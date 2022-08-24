6a15dc4e3f0136f353e9e833a206e95f58903cad
Manar Ali <ahmadshehadeh78@gmail.com>
2022-08-24 18:02:02 +0100

    Added first post

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+struct Post {
+	title: String,
+	author: String,
+	date_time: String,
+	content: String,
+	tag: Tag,
+	reviews: Review	
+}
+
+struct Review {
+	author: String,
+	date_time: String,
+	content: String,
+	rating: f32
+}
+
+enum Tag {
+	Holiday,
+	Programming,
+	Business,
+	Projects,
+	Other
+}
+
+impl Post {
+	fn post(self) {
+		println!("{}", self.reviews.author);
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+   let my_post_a = Post {
+	   	title: "hello".to_string(),
+	   	author: "bye".to_string(),
+	   	date_time: "sigh".to_string(),
+	   	content: "shy".to_string(),
+	   	tag: Tag::Holiday,
+	   	reviews: Review {
+		   		author: "uwu".to_string(),
+		   		date_time: "owo".to_string(),
+		   		content: "hate it!".to_string(),
+		   		rating: 100.0,
+	   	},
+   	};
+
+   	my_post_a.post();
+
 }
