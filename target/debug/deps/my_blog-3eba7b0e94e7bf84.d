84afbefb5925c503bc1e1fb7909f34e64585a130
Manar Ali <ahmadshehadeh78@gmail.com>
2022-08-24 17:37:48 +0100

    Init Commit

---
+++ target/debug/deps/my_blog-3eba7b0e94e7bf84.d
@@ -0,0 +1,5 @@
+/home/bread/my_blog/target/debug/deps/my_blog-3eba7b0e94e7bf84: src/main.rs
+
+/home/bread/my_blog/target/debug/deps/my_blog-3eba7b0e94e7bf84.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/my_blog.d
@@ -0,0 +1,1 @@
+/home/bread/my_blog/target/debug/my_blog: /home/bread/my_blog/src/main.rs
